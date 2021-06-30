1251322bf971174de1deaec93927c322e3c25612
Vardhah Anees <63852903+vardhahanees@users.noreply.github.com>
2021-07-01 01:10:35 +0400

    Update swap.c

--- swap.c
+++ swap.c
@@ -2,18 +2,17 @@
 void swap_max(int arr[], int l, int n)
 {
   int max = arr[n];
-  int temp;
+  int temp, pos=n;
   for (int i=n; i<l; i++)
   {
     if (arr[i] > max)
     {
       max = arr[i];
+      pos = i;
     }
-    if (arr[i]==max)
-    {  
       temp = arr[n];
-      arr[n] = arr[i];
-      arr[i] = temp;
+      arr[n] = max;
+      arr[pos] = temp;
     }
    }
 }
