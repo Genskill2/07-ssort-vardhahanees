1152c901497dfd4168bbe6c275fad52a02a5484c
Vardhah Anees <63852903+vardhahanees@users.noreply.github.com>
2021-07-01 01:15:36 +0400

    Update swap.c

--- swap.c
+++ swap.c
@@ -3,31 +3,28 @@
 
 void swap_max(int arr[], int l, int n)
 {
-int t;
-int great=arr[n];
-for(int i=n;i<l;i++)
-{
-if(arr[i]>great)
-{
-great=arr[i];
-}
-}
-for(int i=n;i<l;i++)
-{
-if(arr[i]==great)
-{
-t=arr[n];
-arr[n]=arr[i];
-arr[i]=t;
-}
-}
+  int t;
+  int max = arr[n];
+  for(int i=n; i<l; i++)
+  {
+    if(arr[i]>great)
+    {
+      max = arr[i];
+    }
+    if(arr[i]==max)
+    {
+      t = arr[n];
+      arr[n] = arr[i];
+      arr[i]  =t;
+    }
+  }
 }
 
 
 void ssort(int arr[], int l)
 {
-for(int i=0;i<l-1;i++)
-{
-swap_max(arr,l,i);
-}
+  for(int i=0; i<l-1; i++)
+  {
+    swap_max(arr,l,i);
+  }
 }
